cb72834d9709c851eef32a5ed6a131f350e892fd
Akarsh Sharma <43448929+Akarsh-Sharma@users.noreply.github.com>
2021-04-04 20:31:41 -0400

    Files for the available dishes for the restaurant.

---
+++ Second phase/src/MenuDishes/Dish.java
@@ -0,0 +1,207 @@
+package MenuDishes;
+
+import Restaurant.DishIngredient;
+import Restaurant.DishStatus;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+
+/**
+ * A dish inherits the properties of a menuitem but is tailored to a table.
+ */
+public class Dish extends MenuItem {
+
+  protected String tableName;
+  protected int customerNum;
+  protected String comment;
+  protected DishStatus dishStatus;
+
+  /**
+   * A dish that a customer orders. The Dishes get passed around the restaurant until the customer
+   * receives it.
+   *
+   * @param dish The food that is on this dish.
+   * @param tableName The name of the table that this dish is served to.
+   * @param customerNum The seat number of the customer at the table.
+   */
+  public Dish(MenuItem dish, String tableName, int customerNum) {
+
+    super(dish.getName(), dish.getId(), dish.getPrice(), dish.getIngredients());
+    this.tableName = tableName;
+    this.customerNum = customerNum;
+    this.comment = "";
+    this.setDishStatus(DishStatus.ORDERED);
+  }
+
+
+  /**
+   * Adds a comment to the dish for the Cook. The server will add specs in the comment that the
+   * customer requests.
+   *
+   * @param comment The Comment.
+   */
+  public void addComment(String comment) {
+    this.comment = comment;
+  }
+
+  /**
+   * Gets the customer number for this dish.
+   *
+   * @return int
+   */
+  public int getCustomerNum() {
+    return customerNum;
+  }
+
+  /**
+   * Returns the string representation of the Bill.
+   *
+   * @return String
+   */
+  public String toString() {
+    String billText = "";
+    billText += "Table: " + this.tableName + ", ";
+    billText += "CustomerNumber: " + this.customerNum + ", ";
+    billText += getStringForBill();
+    return billText;
+  }
+
+  /**
+   * Creates a text representation of the Dish for creating the Bill.
+   *
+   * @return String
+   */
+  public String getStringForBill() {
+    String billText = "";
+    billText += this.getName();
+    if (!this.getComplementsString().equals("")) {
+      billText += " - " + this.getComplementsString();
+    }
+    billText += " - $" + doubleToCurrency(this.getPrice());
+    return billText;
+  }
+
+  /**
+   * Returns a string with each ingredient and the amount.
+   */
+  public String getIngredientString() {
+    String ingredientOutput = "";
+    for (String key : this.getIngredients().keySet()) {
+      ingredientOutput +=
+          key + ": " + this.getIngredients().get(key).getAmount() + System.lineSeparator();
+    }
+    return ingredientOutput;
+  }
+
+  /**
+   * Returns a string with all the complements.
+   */
+  public String getComplementsString() {
+    String extras = "";
+    String subtractions = "";
+    HashMap<String, Integer> differenceMap = this.getDifferenceAmounts();
+
+    for (String key : differenceMap.keySet()) {
+      if (differenceMap.get(key) > 0) {
+        extras +=
+            "+" + differenceMap.get(key) + " " + this.getIngredients().get(key).getName() + ", ";
+      } else if (differenceMap.get(key) < 0) {
+        subtractions +=
+            differenceMap.get(key) + " " + this.getIngredients().get(key).getName() + ", ";
+      }
+    }
+    String complements = "";
+    complements += extras;
+    complements += subtractions;
+    if (complements.length() == 0) {
+      return "";
+    } else {
+      return complements.substring(0, complements.length() - 2);
+    }
+  }
+
+  /**
+   * Getter for tableName.
+   *
+   * @return String tableName.
+   */
+  public String getTableName() {
+    return tableName;
+  }
+
+  /**
+   * Creates a copy of this dish with the table id.
+   *
+   * @param tableName the table that the dish is for
+   * @param customerNumber the customer that ordered the dish
+   * @return the dish that has been ordered
+   */
+  public static Dish getDishFromMenuItem(MenuItem menu, String tableName, int customerNumber) {
+    MenuItem newDish = menu.clone();
+    Dish dish = new Dish(newDish, tableName, customerNumber);
+    return dish;
+  }
+
+  /**
+   * Gets the comment attached to the dish.
+   *
+   * @return the comment.
+   */
+  public String getComment() {
+    return comment;
+  }
+
+  /**
+   * Sets the comment attached to the dish.
+   *
+   * @param comment comment about the dish.
+   */
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+
+  /**
+   * Sets the dish ingredients to back to baseAmounts.
+   */
+  public void setToBaseIngredients() {
+    HashMap<String, DishIngredient> ingredients = this.getIngredients();
+    for (String key : ingredients.keySet()) {
+      DishIngredient ingredient = ingredients.get(key);
+      ingredients.get(key).setAmount(ingredient.getBaseAmount());
+    }
+  }
+
+  /**
+   * Adds necessary 0's to the number. Ex. "3.0" turns into "3.00"
+   */
+  private static String doubleToCurrency(double number) {
+    DecimalFormat decim = new DecimalFormat("0.00");
+    return decim.format(number);
+  }
+
+  /**
+   * Returns a dish's status, i.e. whether it is ordered, cooked, rejected, etc.
+   * @return the dish's status
+   */
+  public DishStatus getDishStatus() {
+    return dishStatus;
+  }
+
+  /**
+   * Sets the dish's status
+   * @param dishStatus the dish's status
+   */
+  public void setDishStatus(DishStatus dishStatus) {
+    this.dishStatus = dishStatus;
+  }
+
+  /**
+   * Returns a dish which is a menuItem along with the table that ordered it and the customer that ordered it
+   * @return
+   */
+  public Dish clone() {
+    MenuItem menuItem = new MenuItem(this.name, this.id, this.price, this.cloneIngredients());
+    return new Dish(menuItem, this.tableName, this.customerNum);
+  }
+
+
+}
---
+++ Second phase/src/MenuDishes/MenuItem.java
@@ -0,0 +1,179 @@
+package MenuDishes;
+
+import Restaurant.DishIngredient;
+import java.util.HashMap;
+
+/**
+ * This is a dish that is displayed in the menu which can be ordered by customers
+ */
+public class MenuItem {
+
+  protected String name; // name of the dish
+  protected double id; // id of the dish
+  protected double price; // price of the dish
+  protected HashMap<String, DishIngredient> ingredients; // the ingredients of this dish.
+
+  /**
+   * This is a food item that is on the Menu.
+   *
+   * @param name The name of the dish
+   * @param id The id assigned to the dis
+   * @param price The price of the dish
+   * @param ingredients The ingredients needed to make the dish
+   */
+  public MenuItem(
+      String name, double id, double price, HashMap<String, DishIngredient> ingredients) {
+    this.name = name;
+    this.id = id;
+    this.price = price;
+    this.ingredients = ingredients;
+  }
+
+  /**
+   * Returns the id of this menu item.
+   *
+   * @return the id of the menu item
+   */
+  public double getId() {
+    return this.id;
+  }
+
+  /**
+   * Creates a non-alias copy of this MenuItem.
+   *
+   * @return MenuItem
+   */
+  public MenuItem clone() {
+    return new MenuItem(this.name, this.id, this.price, cloneIngredients());
+  }
+
+  /**
+   * Makes a non-alias copy of the ingredients.
+   *
+   * @return Hashmap
+   */
+  public HashMap<String, DishIngredient> cloneIngredients() {
+
+    HashMap<String, DishIngredient> copy = new HashMap<>();
+
+    for (String key : this.ingredients.keySet()) {
+      copy.put(key, this.ingredients.get(key).clone());
+    }
+    return copy;
+  }
+
+  /**
+   * Returns the price of the MenuItem.
+   *
+   * @return double
+   */
+  public double getPrice() {
+    double finalPrice = price;
+    for (String key : this.ingredients.keySet()) {
+      DishIngredient dishIngredient = this.ingredients.get(key);
+      int difference = dishIngredient.getAdditionAmount();
+      finalPrice += difference * dishIngredient.getPrice();
+    }
+    return finalPrice;
+  }
+
+  /**
+   * Returns the hashmap for ingredients.
+   *
+   * @return hashmap
+   */
+  public HashMap<String, DishIngredient> getIngredients() {
+    return this.ingredients;
+  }
+
+  /**
+   * Adds an ingredient to the dish. Precondition, the amount you are adding is within bounds of the
+   * ingredient's bounds.
+   *
+   * @param ingredient The ingredient being added to the dish
+   * @param amount The amount of that ingredient that is being added
+   */
+  public void addIngredient(String ingredient, int amount) {
+
+    this.ingredients.get(ingredient).addAmount(amount);
+  }
+
+  /**
+   * Subtracts an ingredient from the dish. Precondition, the amount you are subtracting is within
+   * bounds of the ingredient's bounds.
+   *
+   * @param ingredient the ingredient that is being removed from the dish
+   * @param amount the amount of that ingredient that is being removed
+   */
+  public void subtractIngredient(String ingredient, int amount) {
+    this.ingredients.get(ingredient).subtractAmount(amount);
+  }
+
+  /**
+   * returns a HashMap of the ingredient name and its amount.
+   *
+   * @return HashMap
+   */
+  public HashMap<String, Integer> getIngredientAmounts() {
+    HashMap<String, Integer> ingredientAmounts = new HashMap<>();
+    for (String key : this.ingredients.keySet()) {
+      ingredientAmounts.put(key, this.ingredients.get(key).getAmount());
+    }
+    return ingredientAmounts;
+  }
+
+  /**
+   * returns a HashMap of the ingredient name and its amount.
+   *
+   * @return HashMap
+   */
+  protected HashMap<String, Integer> getDifferenceAmounts() {
+    HashMap<String, Integer> differenceMap = new HashMap<>();
+    for (String key : this.ingredients.keySet()) {
+      int dif = this.ingredients.get(key).getBaseDifference();
+      differenceMap.put(key, dif);
+    }
+    return differenceMap;
+  }
+
+  /**
+   * Getter for the name of the dish.
+   *
+   * @return String name.
+   */
+  public String getName() {
+    return name;
+  }
+
+
+  /**
+   * Precondition: the two hashmaps have the same ingredients
+   *
+   * @param compIngredients hashmap you are comparing to.
+   */
+  public HashMap<String, DishIngredient> getPosDifBetweenTwoIngredientsList(
+      HashMap<String, DishIngredient> compIngredients) {
+    HashMap<String, DishIngredient> differences = new HashMap<>();
+    for (String key : compIngredients.keySet()) {
+      int dif = this.ingredients.get(key).getAmount() - compIngredients.get(key).getAmount();
+      DishIngredient dishIngredientCopy = compIngredients.get(key).clone();
+      if (dif > 0) {
+        dishIngredientCopy.setAmount(dif);
+        differences.put(key, dishIngredientCopy);
+      } else {
+        dishIngredientCopy.setAmount(0);
+        differences.put(key, dishIngredientCopy);
+      }
+    }
+    return differences;
+  }
+
+  /**
+   * Ingredients setter.
+   *
+   * @param setIngredients new ingredients.
+   */
+  public void setIngredients(HashMap<String, DishIngredient> setIngredients) {
+    this.ingredients = setIngredients;
+  }
+}
